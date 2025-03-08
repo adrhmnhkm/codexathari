bf9598e1a920e7af5913669dfafc655852b22b93
adrhmnhkm <adrhmnhkm@gmail.com>
2025-03-08 22:01:45 +0700

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Codex Athari';
+export const SITE_DESCRIPTION = 'Berbagi ilmu dan pemahaman Islam sesuai pemahaman Salafush Shalih';
